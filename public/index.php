a7ca3c85aff1c6fe36fb202e6b9e34d12e8a8c83
dragan002 <draganvujic29@gmail.com>
2023-12-11 18:35:30 +0100

    Load Controllers

--- public/index.php
+++ public/index.php
@@ -1,1 +1,25 @@
-hello
+<?php 
+
+function show($stuff) {
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+function splitUrl() {
+    $URL = $_GET['url'] ?? 'home';
+    $URL = explode('/', $URL);
+    return $URL;
+}
+
+function loadController() {
+    $URL = splitUrl();
+    $filename = "../app/controllers/" .ucfirst($URL[0]) . ".php";
+    if(file_exists($filename)) {
+        require $filename;
+    } else {
+        $filename = "../app/controllers/_404.php";
+        require $filename;
+    }
+}
+loadController();
